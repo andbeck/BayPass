b9c16797f37cb42fd0811d5545ac1be31e89d396
Andrew Beckerman <andrew.beckerman@gmail.com>
2016-08-30 17:37:37 +0100

    Update BayPass Data Prep

--- BaypassSetupCleanGenome.R
+++ BaypassSetupCleanGenome.R
@@ -5,6 +5,7 @@
 # ----------------------------------------------------------------------------------------
 
 library(gdata)
+library(dplyr)
 
 # get original allele frequency data
 
@@ -13,37 +14,27 @@ library(gdata)
 # NEW DATA
 load('~/alan/R/Robj/datUse.clean.Rdata')
 
-# groups by fish/midge
-ac<-bed.obj$allele.counts
-rn<-rownames(ac)[c(1,2,3,4,5,7,8,9)]
-rn2<-paste(rn,"_2",sep="")
+wrk<-filter(datUse, pop!="SoF")
 
-# organise major allele count
-alleleC1 <- rbind(
-	bed.obj$allele.counts[1:2,],
-	bed.obj$allele.counts[4,],
-	bed.obj$allele.counts[3,],
-	bed.obj$allele.counts[c(5,7:9),])
-# Sample Size
-ss <- rbind(
-	bed.obj$sample.size[1:2,],
-	bed.obj$sample.size[4,],
-	bed.obj$sample.size[3,],
-	bed.obj$sample.size[c(5,7:9),])
-# minor allele count
-alleleC2 <- ss	- alleleC1
+wrk2<-select(wrk, 1:10)
 
-# housekeeping
-rownames(alleleC1)<-rn
-rownames(alleleC2)<-rn2
+wrk %>% 
+	group_by(pop) %>%
+		
 
-# checking
-alleleC1[1:8,1:2]
-alleleC2[1:8,1:2]
-ss[1:8,1:2]
 
-# Do it with gdata function interleave
-out.temp<-interleave(alleleC1,alleleC2) # keep it like this because writing to file transposes it, which is what we want
+alleleC1<-data.frame(wrk$dp.down)
+alleleC2<-data.frame(wrk$ad.down)
+
+alleleCounts<-cbind(alleleC1, alleleC2)
+alleleCounts[1:10,]
+
+
+
+
+
+
+
 
 # make the covariate File
 covs<-read.csv("~/baypass_2.1/DaphBayPass/pH_Temp_Lat_8_ponds.csv")
