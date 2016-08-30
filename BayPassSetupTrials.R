da48da3a7fa0e086d67e6dd032b77f199c542eeb
Andrew Beckerman <andrew.beckerman@gmail.com>
2016-08-30 22:26:23 +0100

    trials and tribs data prep

---
+++ BayPassSetupTrials.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(magrittr)
+
+AA<-data.frame(ma = runif(10))
+AA$mb<-1-AA$ma
+
+BB<-data.frame(ma = runif(10))
+BB$mb<-1-BB$ma
+
+AA
+BB
+
+df<-rbind(AA,BB)
+df$code<-rep(c("A","B"), each = 10)
+
+# rbind(
+	# c(AA[1,], BB[1,]),
+	# c(AA[2,], BB[2,])
+	# # etc
+	# )
+
+# YES!
+bind_cols(AA,BB)
+
+# Does not work
+df %>%
+	group_by(code) %>%
+		do(vals = bind_cols(data.frame(.)))
+
+# does work, but retains code columns
+i=1
+out<-bind_cols(split(df, df$code))
+out[1:(i+2)!=(i+2)]
+
+out <- df %>% 
+  group_by(code) %>% 
+  do(each_df = select(., -code)) %$%
+  bind_cols(each_df)
