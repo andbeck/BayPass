4be44eb247efe378cd09817707f23697b3155fe8
Andrew Beckerman <andrew.beckerman@gmail.com>
2016-10-03 23:14:20 +0100

    making baypass interpretation

--- baypass_daph_interp.R
+++ baypass_daph_interp.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(gridExtra)
 library(dplyr)
 library(ggthemes)
+library(qqman)
 
 source('baypass_utils.R') # this is in the repo
 
@@ -17,54 +18,95 @@ par(mfrow = c(2,1))
 corrplot(omega_plot,method = "square", order = 'hclust',mar = c(2,1,1,0))
 plot(hclust(dist(omega_plot), 'complete'))
 
-# This needs to be developed to establish the criteria for detecting XtX outlier
-# DETAIL provided by Gautier
 
-# omega read in above
-pi.params<-as.numeric(read.table("anaprEnvfile__summary_beta_params.out",h=T)[,2]) ##the parmeters of the Beta distribution
-poolsize<-as.numeric(read.table("SAMPLEFILE")) #the original pool sizes
-readcounts<-geno2YN("ALLELEFILE") # the orginal read counts (needed to sample from the observed coverages)
 
-simulate.baypass(omega,nsnp=250000,
-	beta.pi = pi.params,
-	sample.size = poolsize, 
-	coverage=readcounts$NN,
-	pi.maf = 0.01,
-	suffix="pods",
-	remove.fixed.loci = F)
+# # This needs to be developed to establish the criteria for detecting XtX outlier
+# # DETAIL provided by Gautier
+
+# # omega read in above
+# pi.params<-as.numeric(read.table("anaprEnvfile__summary_beta_params.out",h=T)[,2]) ##the parmeters of the Beta distribution
+# poolsize<-as.numeric(read.table("SAMPLEFILE")) #the original pool sizes
+# readcounts<-geno2YN("ALLELEFILE") # the orginal read counts (needed to sample from the observed coverages)
+
+# simulate.baypass(omega,nsnp=250000,
+	# beta.pi = pi.params,
+	# sample.size = poolsize, 
+	# coverage=readcounts$NN,
+	# pi.maf = 0.01,
+	# suffix="pods",
+	# remove.fixed.loci = F)
 
 
 
 # The real games begin here.
 
-# Step 1: use the pods analysis to identify the 99% ile of XtX and betas
+load('BayPassInterpret.RData') # this should eliminate data reading and prep for 
+
+# # Step 1: use the pods analysis to identify the 99% ile of XtX
+# XtX_Emp<-read.table('anaprEnvfile__summary_pi_xtx.out', header = TRUE)
+# XtX_Sim<-read.table('anaPOD__summary_pi_xtx.out', header = TRUE)
+
+# # calcuate the threshold XtX using quantile = 0.99, for the top 1%
+# XtX_threshold<-quantile(XtX_Sim$M_XtX, 0.99)
+# XtX_Sig<-XtX_Emp[XtX_Emp$M_XtX>=XtX_threshold,]
+
+# # percent exceeding threshold
+# dim(XtX_Sig)[1]/dim(XtX_Emp)[1]
+
+# # Step 2: use the pods analysis to identify the 99% ile of betas
+# # The tables are indexed by covariable 1-3
+# # 1 = lat/pH
+# # 2 = temp
+# # 3 = predation
+# beta_Emp<-read.table('anaprEnvfile__summary_betai_reg.out', header = TRUE)
+# beta_Sim<-read.table('anaPOD__summary_betai_reg.out', header = TRUE)
+
+# # the B
+# one<-filter(beta_Emp, COVARIABLE == '1')
+# two<-filter(beta_Emp, COVARIABLE == '2')
+# three<-filter(beta_Emp, COVARIABLE == '3')
+
+# betaThreshold<-beta_Sim %>%
+	# group_by(COVARIABLE) %>%
+	# summarise(
+		# betaThreshold = quantile(BF.dB., 0.99))
+
+# betaThreshold$COVARIABLE<-c("Lat/pH", "Temp", "Predation")
+# betaThreshold
+
+# df_latpH<-data.frame(one, XtX = XtX_Emp$M_XtX)
+# df_temp<-data.frame(two, XtX = XtX_Emp$M_XtX)
+# df_predation<-data.frame(three, XtX = XtX_Emp$M_XtX)
+
+# one_keypts<-filter(df_latpH, BF.dB. > as.numeric(betaThreshold[1,2]) & XtX > XtX_threshold) # 2890
+# two_keypts<-filter(df_temp, BF.dB. > as.numeric(betaThreshold[2,2]) & XtX > XtX_threshold) # 2890
+# three_keypts<-filter(df_predation, BF.dB. > as.numeric(betaThreshold[3,2]) & XtX > XtX_threshold) # 2890
+
+par(mfrow = c(1,3))
+#latitude/pH
+plot(BF.dB. ~ XtX, data = df_latpH, pch = '.', col = 'grey', ylim = c(-10,60))
+abline(v = XtX_threshold, lty = 3, lwd = 2, col = 'red')
+abline(h = betaThreshold[1,2], lty = 3, lwd = 2, col = 'green')
+points(BF.dB. ~ XtX, data = one_keypts, pch = 21, col = 'black')
+title('Latitude/pH')
+
+# temp
+plot(BF.dB. ~ XtX, data = df_temp, pch = '.', col = 'grey', ylim = c(-10,60))
+abline(v = XtX_threshold, lty = 3, lwd = 2, col = 'red')
+abline(h = betaThreshold[2,2], lty = 3, lwd = 2, col = 'green')
+points(BF.dB. ~ XtX, data = two_keypts, pch = 21, col = 'black')
+title('Temperature')
+
+# predation
+plot(BF.dB. ~ XtX, data = df_predation, pch = '.', col = 'grey', ylim = c(-10,60))
+abline(v = XtX_threshold, lty = 3, lwd = 2, col = 'red')
+abline(h = betaThreshold[3,2], lty = 3, lwd = 2, col = 'green')
+points(BF.dB. ~ XtX, data = three_keypts, pch = 21, col = 'black')
+title('Predation')
 
 
-# Step 2: use these as thresholds for identifying SNPS.
-
-
-beta<-read.table('anaprEnvfile__summary_betai_reg.out', header = TRUE)
-XtX<-read.table('anaprEnvfile__summary_pi_xtx.out', header = TRUE)
-
+####
 beta_dB_XtX<-data.frame(beta = beta$Beta_is, dB = beta$BF.dB., XtX = XtX$M_XtX)
 decisive_dB<-filter(beta_dB_XtX, dB>20 & XtX>quantile(beta_dB_XtX$XtX, 0.99))
 strongAssoc<-filter(decisive_dB, beta>=0.2|beta<=c(-0.2))
 
-# these take a long time - ggplot is slow and the criteria are wrong
-p1<-ggplot(beta_dB_XtX, aes(x = XtX, y = dB))+
-	geom_point(size = 0.5, alpha = 0.2)+
-	geom_point(data = decisive_dB, aes(x = XtX, y = dB), size = 3, shape = 1, col = 'red')+
-	geom_hline(yintercept = 20, col = "red", linetype = 'dashed')+
-	geom_vline(xintercept = quantile(beta_dB_XtX$XtX, 0.99), col = "red", linetype = 'dashed')+
-	theme_base()
-
-
-p2<-ggplot(beta_dB_XtX, aes(x = XtX, y = beta))+
-	geom_point(size = 2, alpha = 0.2)+
-	geom_point(data = decisive_dB, aes(x = XtX, y = beta), size = 3, shape = 1,col = 'red')+
-	geom_point(data = strongAssoc, aes(x = XtX, y = beta), size = 3, shape = 1,col = 'green')+
-	geom_hline(yintercept = c(0.2,-0.2), col = "red", linetype = 'dashed')+
-	geom_vline(xintercept = quantile(beta_dB_XtX$XtX, 0.99), col = "red", linetype = 'dashed')+
-	theme_base()
-	
-grid.arrange(p1, p2, nrow = 2)
